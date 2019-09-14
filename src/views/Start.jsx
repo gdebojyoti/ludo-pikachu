e20f09cbe0f415d0be22f2f9c98e6f55387452fd
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2019-09-15 04:45:29 +0530

    Facebook Login - integrated, disabled & Chrome issue. (#3)
    * FB Login; access to user's id & full name. Pending: Chrome issue.
    
    * Login disabled (note added for Chrome issue).
    
    * Login issue comment.

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -1,5 +1,7 @@
 // accept user's name & save it to local storage
 
+// NOTE: FB login is currently working on FF; not in Chrome. Doesn't work in localhost either. Access to name & id by default. Additional request for profile pic required.
+
 import React, { useState } from 'react'
 
 import { setValue } from '../utilities/localStorage'
@@ -7,12 +9,44 @@ import { setValue } from '../utilities/localStorage'
 import '../styles/views/startScreen'
 
 const StartScreen = ({ onEnter }) => {
+  // useEffect(() => {
+  //   document.addEventListener('FbObjectReady', checkLoginState)
+  // }, [])
+
+  // function checkLoginState () {
+  //   console.log('checking...')
+  //   window.FB.getLoginStatus(function (response) {
+  //     console.log(response)
+  //     if (response.status === 'connected') {
+  //       window.FB.login(facebookLoginHandler, {
+  //         // scope: 'public_profile,email'
+  //         scope: 'public_profile'
+  //       })
+  //     }
+  //   })
+  // }
+
+  // const facebookLoginHandler = response => {
+  //   if (response.status === 'connected') {
+  //     window.FB.api('/me', userData => {
+  //       console.log('successful login with data', userData, response)
+  //     })
+  //   } else {
+  //     console.log('data fetching failed', response)
+  //   }
+  // }
+
   const saveId = (id) => {
     setValue('username', id)
     onEnter(id)
   }
 
-  return <Name saveId={saveId} />
+  return <div>
+    <Name saveId={saveId} />
+
+    {/* <button onClick={checkLoginState}>Dummy FB!</button>
+    <div className='fb-login-button' data-width='' data-size='medium' data-button-type='continue_with' data-auto-logout-link='true' data-use-continue-as='true' /> */}
+  </div>
 }
 
 const Name = ({ saveId }) => {
